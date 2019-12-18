d07791f196bb565b1c63f651b1354c25bda5cef9
soso <ethdud1@gmail.com>
2019-12-18 21:23:11 +0900

    Change default theme

--- src/components/theme-switch/index.jsx
+++ src/components/theme-switch/index.jsx
@@ -7,7 +7,7 @@ import { THEME } from '../../constants'
 import './index.scss'
 
 function getTheme(checked) {
-  return checked ? THEME.DARK : THEME.LIGHT
+  return !checked ? THEME.DARK : THEME.LIGHT
 }
 
 function toggleTheme(theme) {
@@ -50,12 +50,12 @@ export const ThemeSwitch = () => {
           id="normal-switch"
           height={24}
           width={48}
-          checkedIcon={<div className="icon checkedIcon">D</div>}
-          uncheckedIcon={<div className="icon uncheckedIcon">L</div>}
-          offColor={'#d9dfe2'}
-          offHandleColor={'#fff'}
-          onColor={'#999'}
-          onHandleColor={'#282c35'}
+          uncheckedIcon={<div className="icon checkedIcon">D</div>}
+          checkedIcon={<div className="icon uncheckedIcon">L</div>}
+          offColor={'#999'}
+          offHandleColor={'#282c35'}
+          onColor={'#d9dfe2'}
+          onHandleColor={'#fff'}
         />
       </label>
     </div>
