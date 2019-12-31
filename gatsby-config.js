74e0e36f74ac23128e643fd5bf86d1bf9711595b
soso <ethdud1@gmail.com>
2019-12-31 11:13:38 +0900

    Remove site

--- gatsby-config.js
+++ gatsby-config.js
@@ -100,7 +100,6 @@ module.exports = {
         feeds: [{
           serialize: ({
               query: {
-                site,
                 allMarkdownRemark
               }
             }) =>
