b13f3ae43cecb43c2c42a19e8b429d841b4580e0
soso <ethdud1@gmail.com>
2020-02-15 19:08:03 +0900

    Remove remark image max

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,11 +27,7 @@ module.exports = {
       resolve: `gatsby-transformer-remark`,
       options: {
         plugins: [{
-            resolve: `gatsby-remark-images`,
-            options: {
-              maxWidth: 590,
-              linkImagesToOriginal: false,
-            },
+            resolve: `gatsby-remark-images`
           },
           {
             resolve: `gatsby-remark-images-medium-zoom`,
