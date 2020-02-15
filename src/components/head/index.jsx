01bc277c3b12be8ee035f5c3a9f86a17e4a57835
soso <ethdud1@gmail.com>
2020-02-15 18:31:02 +0900

    Test

--- src/components/head/index.jsx
+++ src/components/head/index.jsx
@@ -36,7 +36,7 @@ export function Head({
               },
               {
                 property: `og:image`,
-                content: `${data.site.siteMetadata.siteUrl}${thumbnail}`
+                content: `https://deploy-preview-36--friendly-bose-75c3ce.netlify.com${thumbnail}`
               },
               {
                 property: `og:description`,
