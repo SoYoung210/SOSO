0321ca7b6fa8edf6965faead85ea9953b942ffad
soso <ethdud1@gmail.com>
2020-02-15 21:01:33 +0900

    Fix typo

--- src/components/head/index.jsx
+++ src/components/head/index.jsx
@@ -36,7 +36,7 @@ export function Head({
               },
               {
                 property: `og:image`,
-                content: `${data.site.siteMetadata.siteUrl}{${thumbnail}`
+                content: `${data.site.siteMetadata.siteUrl}${thumbnail}`
               },
               {
                 property: `og:description`,
