92b3c0ca22cebca261b2114ae35f94d8700195e3
SoYoung210 <ethdud1@gmail.com>
2022-04-01 11:02:49 +0900

    fix: profile

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   title: `SOSOLOG`,
   description: `Blog posted about ...`,
-  author: `SO_YOUNG`,
-  introduction: `📝 소소하게 끄적이는 개발로그`,
+  author: `Jbee`,
+  introduction: `Web Engineer Interested in 설계.테스트.생산성.자동화.멘토링. FEConf Organizer @FEDG`,
   siteUrl: `https://so-so.dev`, // Your blog site url
   social: {
     twitter: `th096`, // Your Twitter account
