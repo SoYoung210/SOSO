a96cef00bd576708a88e2727efe1e820597be3fa
soso <ethdud1@gmail.com>
2023-01-24 16:14:08 +0900

    chore: add sponsor button (#110)
    * chore: add sponsor button

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -8,6 +8,7 @@ import { PostTitle } from '../components/post-title'
 import { PostDate } from '../components/post-date'
 import { PostContainer } from '../components/post-container'
 import { SocialShare } from '../components/social-share'
+import { SponsorButton } from '../components/sponsor-button'
 import { Bio } from '../components/bio'
 import { PostNavigator } from '../components/post-navigator'
 import { Disqus } from '../components/disqus'
@@ -43,9 +44,7 @@ export default ({ data, pageContext, location }) => {
       <PostDate date={date} />
       <PostContainer html={post.html} />
       <SocialShare title={title} author={author} social={social} />
-      {/* {!!sponsor.buyMeACoffeeId && (
-        <SponsorButton sponsorId={sponsor.buyMeACoffeeId} />
-      )} */}
+      <SponsorButton />
       <Elements.Hr />
       <Bio />
       <PostNavigator pageContext={pageContext} />
