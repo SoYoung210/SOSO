5d4b9b73e1588915035c7daa0036bf0c117c0c47
SoYoung210 <ethdud1@gmail.com>
2023-01-25 13:47:16 +0900

    fix: post container style

--- src/components/post-container/index.jsx
+++ src/components/post-container/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
+import './index.scss'
 
 export const PostContainer = ({ html }) => (
-  <div dangerouslySetInnerHTML={{ __html: html }} />
+  <div className="post-container" dangerouslySetInnerHTML={{ __html: html }} />
 )
